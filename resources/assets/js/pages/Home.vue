72f06238b0d7e8774f7e888eccf418e4c4967cb5
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-06-14 21:48:07 +0300

    feature (Home.vue) output page create new employee

--- resources/assets/js/pages/Home.vue
+++ resources/assets/js/pages/Home.vue
@@ -3,10 +3,71 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card card-default">
-                    <div class="card-header">Example Component</div>
+                    <div class="card-header">{{ $t('translation.createNewEmployeeTitle') }}</div>
 
                     <div class="card-body">
-                        I'm an example component.
+                        <div class="mb-5">
+                            <div class="form-group">
+                                <label for="surname"> {{ $t('translation.surname') }} </label>
+                                <input type="text" class="form-control" id="surname"
+                                       v-model="surname">
+                            </div>
+                            <div class="form-group">
+                                <label for="name"> {{ $t('translation.name') }} </label>
+                                <input type="text" class="form-control" id="name"
+                                       v-model="name">
+                            </div>
+                            <div class="form-group">
+                                <label for="middle_name"> {{ $t('translation.middleName') }} </label>
+                                <input type="text" class="form-control" id="middle_name"
+                                       v-model="middleName">
+                            </div>
+
+                            <div class="form-group">
+                                <label for="photo"> {{ $t('translation.addPhoto') }} </label>
+                                <input type="file" name="photo" id="photo" class="form-control"
+                                       accept="image/*"
+                                       @change="photoChange($event.target.files)">
+                            </div>
+                        </div>
+                        <div class="mb-5">
+                            <div class="form-group row">
+                                <label for="sociability" class="col-sm-4 col-form-label">{{ $t('translation.sociability') }}</label>
+                                <div class="col-sm-2">
+                                    <input type="number" min="1" max="10" class="form-control" id="sociability"
+                                           v-model="sociability">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="engineeringSkills" class="col-sm-4 col-form-label">{{ $t('translation.engineeringSkills') }}</label>
+                                <div class="col-sm-2">
+                                    <input type="number" min="1" max="10" class="form-control" id="engineeringSkills"
+                                           v-model="engineeringSkills">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="timeManagement" class="col-sm-4 col-form-label">{{ $t('translation.timeManagement') }}</label>
+                                <div class="col-sm-2">
+                                    <input type="number" min="1" max="10" class="form-control" id="timeManagement"
+                                           v-model="timeManagement">
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label for="knowledgeLanguages" class="col-sm-4 col-form-label">{{ $t('translation.knowledgeLanguages') }}</label>
+                                <div class="col-sm-2">
+                                    <input type="number" min="1" max="10" class="form-control" id="knowledgeLanguages"
+                                           v-model="knowledgeLanguages">
+                                </div>
+                            </div>
+                        </div>
+                        <hr>
+                        <div class="form-group">
+                            <label for="selectProject">{{ $t('translation.selectProject') }}</label>
+                            <multiselect id="selectProject"
+                                    v-model="selected"
+                                    :options="options">
+                            </multiselect>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -15,9 +76,34 @@
 </template>
 
 <script>
+    import Multiselect from 'vue-multiselect';
+
     export default {
+        components: {
+            Multiselect,
+        },
+        data() {
+            return {
+                surname: '',
+                name: '',
+                middleName: '',
+                sociability: '5',
+                engineeringSkills: '5',
+                timeManagement: '5',
+                knowledgeLanguages: '5',
+                selected: null,
+                options: ['1', '2', '3'],
+            };
+        },
         mounted() {
             console.log('Component mounted Home.')
-        }
-    }
+        },
+        methods: {
+            photoChange(file) {
+                this.file = file[0];
+            },
+        },
+    };
 </script>
+
+<style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
