8fefe8a8a5ae339ce3c820a26e0447d1fa17bd8e
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-06-14 23:06:32 +0300

    feature (Seeds) start

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\ListItem;
+use App\ListLabel;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,6 +13,74 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        $list_labels = [
+            "characteristics",
+            "projects",
+        ];
+
+
+        $id = 1;
+        foreach ($list_labels as $item) {
+
+            $filed = ListLabel::find($id);
+
+            if (is_null($filed)) {
+
+                $field = new ListLabel();
+                $field->id = $id++;
+                $field->label = $item;
+                $field->save();
+            }
+        }
+
+        $characteristics = [
+            [
+                "label" => "Sociability",
+                "value" => "sociability",
+            ],[
+                "label" => "Engineering Skills",
+                "value" => "engineeringSkills",
+            ],[
+                "label" => "Time Management",
+                "value" => "timeManagement",
+            ],[
+                "label" => "Knowledge of languages",
+                "value" => "knowledgeLanguages",
+            ],
+        ];
+
+        $id = 1;
+        $characteristics_id = 1;
+        foreach ($characteristics as $item) {
+
+            $filed = ListItem::find($id);
+
+            if (is_null($filed)) {
+
+                $field = new ListItem();
+                $field->id = $id;
+                $field->list_label_id = $characteristics_id;
+                $field->label = $item["label"];
+                $field->value = $item["value"];
+                $field->save();
+            }
+            $id++;
+        }
+
+        $projects_id = 2;
+        for ($key = 1; $id <= 19; $key++, $id++) {
+
+            $filed = ListItem::find($id);
+
+            if (is_null($filed)) {
+
+                $field = new ListItem();
+                $field->id = $id;
+                $field->list_label_id = $projects_id;
+                $field->label = "Проект " . $key;
+                $field->save();
+            }
+        }
+
     }
 }
