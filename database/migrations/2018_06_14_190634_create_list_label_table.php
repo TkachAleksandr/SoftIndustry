55071603dea820fb12cd9b32d04964b97996eebc
Tkach Aleksandr <o.tkach@linet.sumdu.edu.ua>
2018-06-14 22:32:27 +0300

    feature (Migrations) start

--- database/migrations/2018_06_14_190634_create_list_label_table.php
+++ database/migrations/2018_06_14_190634_create_list_label_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateListLabelTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('list_label', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('surname', 255);
-            $table->string('name', 255);
-            $table->string('middle_name', 255);
-            $table->string('photo_path')->nullable()->default(null);
+            $table->string('value', 255)->nullable()->default(null);
+            $table->string('label', 255);
             $table->timestamps();
         });
     }
@@ -30,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('list_label');
     }
 }
---
+++ database/migrations/2018_06_14_190646_create_list_items_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateListItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list_items', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('list_label_id')->unsigned();
+            $table->string('value', 255)->nullable()->default(null);
+            $table->string('label', 255);
+            $table->timestamps();
+        });
+
+        Schema::table('list_items', function (Blueprint $table) {
+            $table->foreign("list_label_id")->references("id")->on("list_label")->onUpdate("cascade");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list_items');
+    }
+}
